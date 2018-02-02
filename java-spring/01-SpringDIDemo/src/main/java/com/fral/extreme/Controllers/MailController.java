75ca2d4cb02a29521e19bc64e57183f7421f37d7
franco-extreme <franck_ral@hotmail.com>
2018-02-02 17:33:48 -0400

    Java Examples - Spring Dependency Injection Demo - Learning INJECTING BEANS, resolve issues when there are more than a BEAN in the application context

--- java-spring/01-SpringDIDemo/src/main/java/com/fral/extreme/Controllers/MailController.java
+++ java-spring/01-SpringDIDemo/src/main/java/com/fral/extreme/Controllers/MailController.java
@@ -8,8 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MailController {
 
-    @Autowired
+    //@Autowired
     private MailSender mailSender;
+
+
+    /**
+     * This also work as previous example. It is also going to take mail sender from
+     * application problems.
+     *
+     * We may have problems when there are more than a bean from MailSender, there are many ways
+     * for fixing this.
+     *
+     * 1. Add the same name of the class that we want to use, in camel case.
+     *
+     * @Autowired
+     * public void setMailSender(MailSender smptMailSender) {}
+     *
+     * 2. According the name that is set in component annotation.
+     * @Component("smtp")
+     * public class SmtpMailSender {}
+     *
+     * */
+    @Autowired
+    public void setMailSender(MailSender smtpMailSender) {
+        this.mailSender = smtpMailSender;
+    }
 
     @RequestMapping("/mail")
     public String mail() {
