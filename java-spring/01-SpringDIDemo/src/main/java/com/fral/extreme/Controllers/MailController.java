b5d3db3b27d4ce101e5df83e873086a26b3dbf22
franco-extreme <franck_ral@hotmail.com>
2018-02-02 17:16:14 -0400

    Java Examples - Spring Dependency Injection Demo - Learning basic concepts of dependency injection, and applied the first changes

--- java-spring/01-SpringDIDemo/src/main/java/com/fral/extreme/Controllers/MailController.java
+++ java-spring/01-SpringDIDemo/src/main/java/com/fral/extreme/Controllers/MailController.java
@@ -1,14 +1,15 @@
 package com.fral.extreme.Controllers;
 
 import com.fral.extreme.Mail.MailSender;
-import com.fral.extreme.Mail.MockMailSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MailController {
 
-    private MailSender mailSender = new MockMailSender();
+    @Autowired
+    private MailSender mailSender;
 
     @RequestMapping("/mail")
     public String mail() {
