a01ed3ce9724e626a7db69a0f4f390f7482b6262
franco-extreme <franck_ral@hotmail.com>
2018-02-04 14:52:40 -0400

    JavaUnitTests-JUnit4-Testing Performance in JUnit tests.

--- java-unittests/src/test/java/com/fral/extreme/junit/helper/ArraysCompareTest.java
+++ java-unittests/src/test/java/com/fral/extreme/junit/helper/ArraysCompareTest.java
@@ -26,5 +26,15 @@ public class ArraysCompareTest {
 				
 		Arrays.sort(numbers);		
 	}
+	
+	@Test(timeout=10)
+	public void testSort_Performance() {
+		
+		int array[] = {12, 23, 4};
+		for (int i = 0; i < 1000000; i++) {
+			array[0] = i;
+			Arrays.sort(array);
+		}
+	}
 
 }
