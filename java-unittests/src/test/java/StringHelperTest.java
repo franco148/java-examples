5445923cd79b8a9c6d1964bf6999d7fdce7bbaaf
franco-extreme <franck_ral@hotmail.com>
2018-02-04 11:57:23 -0400

    JavaUnitTests-JUnit-First successful JUnit Success Test and assertEquals

--- java-unittests/src/test/java/StringHelperTest.java
+++ java-unittests/src/test/java/StringHelperTest.java
@@ -2,10 +2,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.fral.extreme.junit.helper.StringHelper;
+
 class StringHelperTest {
 
 	@Test
 	void test() {
+		StringHelper helper = new StringHelper();
+		
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
 	}
 
 }
