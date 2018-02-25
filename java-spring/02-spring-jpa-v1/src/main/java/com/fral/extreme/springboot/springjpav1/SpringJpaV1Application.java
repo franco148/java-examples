d7ddb1e2ea5b33c25e691cf101c99696d3766c68
franco-extreme <franck_ral@hotmail.com>
2018-02-25 19:28:46 -0400

    JavaSpring-GettingStarted: Creating a simple REST Controller

---
+++ java-spring/02-spring-jpa-v1/src/main/java/com/fral/extreme/springboot/springjpav1/SpringJpaV1Application.java
@@ -0,0 +1,12 @@
+package com.fral.extreme.springboot.springjpav1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaV1Application.class, args);
+	}
+}
