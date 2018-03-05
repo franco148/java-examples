5a5f73a8eda430ed49ee216866d597b4bf73a3dd
franco-extreme <franck_ral@hotmail.com>
2018-03-05 16:40:17 -0400

    JavaSpringJpa-GettingStarted: Initial structure of the project.

---
+++ java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/SpringJpaV2Application.java
@@ -0,0 +1,12 @@
+package com.fral.extreme.sbjpa.springjpav2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaV2Application.class, args);
+	}
+}
---
+++ java-spring/03-spring-jpa-v2/src/test/java/com/fral/extreme/sbjpa/springjpav2/SpringJpaV2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fral.extreme.sbjpa.springjpav2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJpaV2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
