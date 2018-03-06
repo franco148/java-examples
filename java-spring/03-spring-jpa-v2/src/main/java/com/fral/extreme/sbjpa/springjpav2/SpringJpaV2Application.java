03fe7e654ef42cbcda139c2a67e9463546998342
franco-extreme <franck_ral@hotmail.com>
2018-03-06 18:31:55 -0400

    JavaSpringJpa-GettingStarted: Implementing insert and update Spring JDBC Update Methods.

--- java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/SpringJpaV2Application.java
+++ java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/SpringJpaV2Application.java
@@ -1,5 +1,6 @@
 package com.fral.extreme.sbjpa.springjpav2;
 
+import com.fral.extreme.sbjpa.springjpav2.entity.Person;
 import com.fral.extreme.sbjpa.springjpav2.jdbc.PersonJdbcDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 /**
  * CommandLineRunner: the implementation is lunch as soon as the application is ready.
@@ -31,5 +34,9 @@ public class SpringJpaV2Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		logger.info("All users -> {}", dao.findAll());
 		logger.info("Users id 1003 -> {}", dao.findById(1003));
+		logger.info("Deleting id 1001 -> Number of rows deleted - {}", dao.deleteById(1001));
+
+		logger.info("Inserting id 1004 -> {}", dao.save(new Person(1004, "Marco Cardenas", "Sucre-Bolivia", new Date())));
+		logger.info("Updating id 1002 -> {}", dao.udpate(new Person(1002, "Fernando Arratia", "Cocha-Bolivia", new Date())));
 	}
 }
--- java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/jdbc/PersonJdbcDao.java
+++ java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/jdbc/PersonJdbcDao.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -28,5 +29,32 @@ public class PersonJdbcDao {
         return jdbcTemplate.queryForObject("select * from person where id=?",
                                             new Object[]{id},
                                             new BeanPropertyRowMapper<Person>(Person.class));
+    }
+
+    //This as a result returns how many rows were affected with the query.
+    public int deleteById(int id) {
+        return jdbcTemplate.update("DELETE from person where id=?", new Object[]{id});
+    }
+
+    public int save(Person person) {
+        return jdbcTemplate.update("INSERT INTO PERSON (id, name, location, birth_date) VALUES (?, ?, ?, ?)",
+                                   new Object[] {
+                                        person.getId(),
+                                        person.getName(),
+                                        person.getLocation(),
+                                        new Timestamp(person.getBirthDate().getTime())
+        });
+    }
+
+    public int udpate(Person person) {
+        return jdbcTemplate.update("UPDATE PERSON " +
+                                        "SET name=?, location=?, birth_date=?" +
+                                        "WHERE id=?",
+                new Object[] {
+                        person.getName(),
+                        person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime()),
+                        person.getId()
+                });
     }
 }
