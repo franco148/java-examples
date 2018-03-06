89ad21c89228a38704a1ebb8a6126bd87f0011d8
franco-extreme <franck_ral@hotmail.com>
2018-03-06 17:51:34 -0400

    JavaSpringJpa-GettingStarted: Execute the findAll method using CommandLineRunner.

--- java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/SpringJpaV2Application.java
+++ java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/SpringJpaV2Application.java
@@ -1,12 +1,30 @@
 package com.fral.extreme.sbjpa.springjpav2;
 
+import com.fral.extreme.sbjpa.springjpav2.jdbc.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * CommandLineRunner: the implementation is lunch as soon as the application is ready.
+ */
 @SpringBootApplication
-public class SpringJpaV2Application {
+public class SpringJpaV2Application implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaV2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users -> {}", dao.findAll());
 	}
 }
