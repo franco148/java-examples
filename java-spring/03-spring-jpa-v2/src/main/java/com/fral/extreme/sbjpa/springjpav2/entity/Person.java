27200bd336b08b8e941957f8512018a97b145a74
franco-extreme <franck_ral@hotmail.com>
2018-03-06 22:03:00 -0400

    JavaSpringJpa-GettingStarted: Defining Person Entity.

--- java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/entity/Person.java
+++ java-spring/03-spring-jpa-v2/src/main/java/com/fral/extreme/sbjpa/springjpav2/entity/Person.java
@@ -1,15 +1,27 @@
 package com.fral.extreme.sbjpa.springjpav2.entity;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+//@Table(name = "Person") //This is not required if the table has already the same name.
 public class Person {
 
+    @Id
+    @GeneratedValue
     private int id;
+    //@Column(name = "name") //This is not required since the table has the same column name.
     private String name;
     private String location;
     private Date birthDate;
 
     public Person() {
+    }
+
+    public Person(String name, String location, Date birthDate) {
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
     }
 
     public Person(int id, String name, String location, Date birthDate) {
