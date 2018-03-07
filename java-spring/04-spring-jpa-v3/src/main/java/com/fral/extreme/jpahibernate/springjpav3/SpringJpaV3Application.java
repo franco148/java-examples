661e0a26fdd467146f728b625d4b20fa2d528fc7
franco-extreme <franck_ral@hotmail.com>
2018-03-06 22:57:10 -0400

    JavaSpringJpa-Hibernate: Getting started with JPAHibernate project.

---
+++ java-spring/04-spring-jpa-v3/src/main/java/com/fral/extreme/jpahibernate/springjpav3/SpringJpaV3Application.java
@@ -0,0 +1,12 @@
+package com.fral.extreme.jpahibernate.springjpav3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaV3Application.class, args);
+	}
+}
