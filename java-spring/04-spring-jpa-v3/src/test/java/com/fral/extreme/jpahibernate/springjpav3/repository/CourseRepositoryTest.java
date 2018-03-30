66b99ee9396da6e81f063215029719aaa94fa524
franco-extreme <franck_ral@hotmail.com>
2018-03-30 12:10:33 -0400

    JavaSpringJpa-Hibernate: JPA&Hibernate - Writing Unit Test for findById method.

---
+++ java-spring/04-spring-jpa-v3/src/test/java/com/fral/extreme/jpahibernate/springjpav3/repository/CourseRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.fral.extreme.jpahibernate.springjpav3.repository;
+
+import com.fral.extreme.jpahibernate.springjpav3.SpringJpaV3Application;
+import com.fral.extreme.jpahibernate.springjpav3.entity.Course;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//Or specify the context that we want to test.
+//@SpringBootTest(classes = SpringJpaV3Application.class)
+public class CourseRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    public void findById() {
+
+        Course course = courseRepository.findById(10001L);
+
+        assertEquals("JPA in 50 steps", course.getName());
+
+        //logger.info("Testing is running...");
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+}
