0bd3f18df8b4054634121bafd88be995fd679d9e
franco-extreme <franck_ral@hotmail.com>
2018-04-01 00:10:30 -0400

    JavaSpringJpa-Hibernate-OneToOne: Jpa&Hibernate - Defining Entieies, Student, Passprt and Review.

--- java-spring/04-spring-jpa-v3/src/main/resources/data.sql
+++ java-spring/04-spring-jpa-v3/src/main/resources/data.sql
@@ -9,3 +9,33 @@ VALUES (10003, 'Spring Framework in 50 steps', sysdate(), sysdate());
 
 INSERT INTO course(id, name, created_date, last_updated_date)
 VALUES (10004, 'Angular in 50 steps', sysdate(), sysdate());
+
+-- --------------------------- STUDENT -----------------------------------
+INSERT INTO student (id, name)
+VALUES (20001, 'Franco Arratia');
+
+INSERT INTO student (id, name)
+VALUES (20002, 'Alvaro Gutierrez');
+
+INSERT INTO student (id, name)
+VALUES (20003, 'Carlos Medinacelli');
+
+-- --------------------------- PASSPORT -----------------------------------
+INSERT INTO passport (id, number)
+VALUES (30001, 'BO3343456');
+
+INSERT INTO passport (id, number)
+VALUES (30002, 'BO0098343');
+
+INSERT INTO passport (id, number)
+VALUES (30003, 'BO3309898');
+
+-- --------------------------- REVIEW -----------------------------------
+INSERT INTO review (id, rating, description)
+VALUES (40001, '5', 'Great Course');
+
+INSERT INTO review (id, rating, description)
+VALUES (40002, '4', 'Awesome course');
+
+INSERT INTO review (id, rating, description)
+VALUES (40003, '5', 'Wonderful course');
