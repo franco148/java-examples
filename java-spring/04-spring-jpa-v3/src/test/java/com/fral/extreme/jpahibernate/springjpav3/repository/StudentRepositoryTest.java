4ca26960f0b5b58ce0c5ecddbee7611af8e06076
franco-extreme <franck_ral@hotmail.com>
2018-04-03 11:16:19 -0400

    JavaSpringJpa-Hibernate-OneToOne: JPA&Hibernate - One to One mapping - Retrieving Student with Passport and Eager Fetch.

---
+++ java-spring/04-spring-jpa-v3/src/test/java/com/fral/extreme/jpahibernate/springjpav3/repository/StudentRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.fral.extreme.jpahibernate.springjpav3.repository;
+
+import com.fral.extreme.jpahibernate.springjpav3.entity.Course;
+import com.fral.extreme.jpahibernate.springjpav3.entity.Student;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//Or specify the context that we want to test.
+//@SpringBootTest(classes = SpringJpaV3Application.class)
+public class StudentRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private StudentRepository courseRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    public void retrieveStudentsAndPassportDetails() {
+
+        Student student = entityManager.find(Student.class, 20001L);
+        logger.info("Student -> {}", student);
+
+        logger.info("passport -> {}", student.getPassport());
+    }
+}
