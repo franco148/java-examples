9c1c17555351888d8dfae4c004ff3ad572781e46
franco-extreme <franck_ral@hotmail.com>
2018-04-08 20:34:36 -0400

    JavaSpringJpa-SpringData: Spring Data REST.

--- java-spring/04-spring-jpa-v3/src/main/java/com/fral/extreme/jpahibernate/springjpav3/repository/CourseSpringDataRepository.java
+++ java-spring/04-spring-jpa-v3/src/main/java/com/fral/extreme/jpahibernate/springjpav3/repository/CourseSpringDataRepository.java
@@ -3,23 +3,25 @@ package com.fral.extreme.jpahibernate.springjpav3.repository;
 import com.fral.extreme.jpahibernate.springjpav3.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 //@Repository
+@RepositoryRestResource(path = "courses")
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
 
     List<Course> findByName(String name);
-    List<Course> findByIdDesc(String name);
-    List<Course> countByName(String name);
-
-    @Query("SELECT c FROM Course c WHERE name LIKE  '%100 steps'")
-    List<Course> coursesWith100Steps(String name);
-
-    @Query(value = "SELECT c FROM Course c WHERE name LIKE  '%100 steps'", nativeQuery = true)
-    List<Course> coursesWith100StepsUsingNativeQuery(String name);
-
-    @Query("query_get_all_courses")
-    List<Course> coursesWith100StepsInNaveUsingNamedQuery(String name);
+//    List<Course> findByIdDesc(String name);
+//    List<Course> countByName(String name);
+//
+//    @Query("SELECT c FROM Course c WHERE name LIKE  '%100 steps'")
+//    List<Course> coursesWith100Steps(String name);
+//
+//    @Query(value = "SELECT c FROM Course c WHERE name LIKE  '%100 steps'", nativeQuery = true)
+//    List<Course> coursesWith100StepsUsingNativeQuery(String name);
+//
+//    @Query("query_get_all_courses")
+//    List<Course> coursesWith100StepsInNaveUsingNamedQuery(String name);
 }
