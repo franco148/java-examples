d8d347577938c09b22d69793060fb44c39123fd2
Franco Arratia <franz.arratia@avantica.net>
2018-11-09 17:42:32 -0400

    FeignClient-ConsumeRestApi: First RestTemplate implementation taking into account BasicAuthentication.

--- java-spring/06-spring-rest-template/src/main/java/com/eextreme/poc/eg01/GetWithBasicAuthExample.java
+++ java-spring/06-spring-rest-template/src/main/java/com/eextreme/poc/eg01/GetWithBasicAuthExample.java
@@ -16,34 +16,34 @@ public class GetWithBasicAuthExample {
     static final String URL_JIRA_SERVER = "https://springjiratest.atlassian.net";
     //endregion
 
-//    public static void main(String[] args) {
-//        // HttpHeaders
-//        HttpHeaders headers = new HttpHeaders();
-//
-//        //
-//        // Authentication
-//        //
-//        String auth = USER_NAME+":"+PASSWORD;
-//        byte[] encodedAuth = Base64.decodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-//        String authHeader = "Basic " + new String(encodedAuth);
-//        headers.set("Authorization", "Basic cm9iZXJ0X2FsXzE2XzJAaG90bWFpbC5jb206RnJhbmNvUm9iZXJ0MTQ4");
-//
-//        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
-//
-//        // Request to return JSON format
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.set("my_other_key", "my_other_value");
-//
-//        // To get result as String
-//        HttpEntity<Object> entity = new HttpEntity<>(headers);
-//
-//        // RestTemplate
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        // Send request with GET method, and Headers.
-//        ResponseEntity<Object> response = restTemplate.exchange(URL_JIRA_SERVER + "/rest/api/3/project", HttpMethod.GET, entity, Object.class);
-//        Object result = response.getBody();
-//
-//        System.out.println(result);
-//    }
+    public static void main(String[] args) {
+        // HttpHeaders
+        HttpHeaders headers = new HttpHeaders();
+
+        //
+        // Authentication
+        //
+        String auth = USER_NAME+":"+PASSWORD;
+        byte[] encodedAuth = Base64.decodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+        String authHeader = "Basic " + new String(encodedAuth);
+        headers.set("Authorization", "Basic cm9iZXJ0X2FsXzE2XzJAaG90bWFpbC5jb206RnJhbmNvUm9iZXJ0MTQ4");
+
+        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+
+        // Request to return JSON format
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("my_other_key", "my_other_value");
+
+        // To get result as String
+        HttpEntity<Object> entity = new HttpEntity<>(headers);
+
+        // RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Send request with GET method, and Headers.
+        ResponseEntity<Object> response = restTemplate.exchange(URL_JIRA_SERVER + "/rest/api/3/project", HttpMethod.GET, entity, Object.class);
+        Object result = response.getBody();
+
+        System.out.println(result);
+    }
 }
