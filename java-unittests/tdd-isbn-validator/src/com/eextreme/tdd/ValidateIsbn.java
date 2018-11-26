72fd62e413b4e908ce69f76bcf3611eb336c8f06
franco-extreme <franck_ral@hotmail.com>
2018-11-25 22:11:55 -0400

    JavaExamples-TddImplementation: Getting started with refactoring extracting constants.

--- java-unittests/tdd-isbn-validator/src/com/eextreme/tdd/ValidateIsbn.java
+++ java-unittests/tdd-isbn-validator/src/com/eextreme/tdd/ValidateIsbn.java
@@ -9,51 +9,63 @@ package com.eextreme.tdd;
  */
 public class ValidateIsbn {
 
+	private static final int LONG_ISBN_MULTIPLIER = 10;
+	private static final int SHORT_ISBN_MULTIPLIER = 11;
+	private static final int SHORT_ISBN_LENGTH = 10;
+	private static final int LONG_ISBN_LENGTH = 13;
+
 	public boolean checkIsbn(String isbnNumber) {
 		
 		//Adding a new feature. 13 digits number ISBN should be valid
-		if (isbnNumber.length() == 13) {
-			int total = 0;
-			
-			for (int i = 0; i < 13; i++) {
-				if (i % 2 == 0) {
-					total += Character.getNumericValue(isbnNumber.charAt(i));
-				} else {
-					total += Character.getNumericValue(isbnNumber.charAt(i)) * 3;
-				}
-			}
-			
-			return total % 10 == 0;
+		if (isbnNumber.length() == LONG_ISBN_LENGTH) {
+			return isThisAValid13DigitIsbn(isbnNumber);
 		} else {
 			// This was implemented when I needed to verify the number of digits of a ISBN number.
-			if (isbnNumber.length() != 10) {
+			if (isbnNumber.length() != SHORT_ISBN_LENGTH) {
 				throw new NumberFormatException("ISBN numbers must be 10 digits long.");
 			}
-					
-
-			/*
-			 * REMEMBER: The quality of this code is not important yet. It is the tests that are important, and as we build
-			 * them up this code will improve as we add more tests and refactor.
-			 */
-			int total = 0;
 			
-			for (int i = 0; i < 10; i++) {
-				if (!Character.isDigit(isbnNumber.charAt(i))) {
-					if (i == 9 && isbnNumber.charAt(i) == 'X') {
-						// This scenario is ok
-						// According the rules, when the last number is X, it will be replaced by 10
-						total += 10;
-					} else {
-						throw new NumberFormatException("ISBN numbers can only contain numeric digits");					
-					}
-				} else {
-					total += Character.getNumericValue(isbnNumber.charAt(i)) * (10 - i);
-				}			
-			}
-			
-			return total % 11 == 0;
+			return isThisAValid10DigitsIsbn(isbnNumber);
 		}
 		
+	}
+
+	private boolean isThisAValid10DigitsIsbn(String isbnNumber) {
+		/*
+		 * REMEMBER: The quality of this code is not important yet. It is the tests that are important, and as we build
+		 * them up this code will improve as we add more tests and refactor.
+		 */
+		int total = 0;
+		
+		for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
+			if (!Character.isDigit(isbnNumber.charAt(i))) {
+				if (i == 9 && isbnNumber.charAt(i) == 'X') {
+					// This scenario is ok
+					// According the rules, when the last number is X, it will be replaced by 10
+					total += 10;
+				} else {
+					throw new NumberFormatException("ISBN numbers can only contain numeric digits");					
+				}
+			} else {
+				total += Character.getNumericValue(isbnNumber.charAt(i)) * (SHORT_ISBN_LENGTH - i);
+			}			
+		}
+		
+		return total % SHORT_ISBN_MULTIPLIER == 0;
+	}
+
+	private boolean isThisAValid13DigitIsbn(String isbnNumber) {
+		int total = 0;
+		
+		for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
+			if (i % 2 == 0) {
+				total += Character.getNumericValue(isbnNumber.charAt(i));
+			} else {
+				total += Character.getNumericValue(isbnNumber.charAt(i)) * 3;
+			}
+		}
+		
+		return total % LONG_ISBN_MULTIPLIER == 0;
 	}
 
 }
