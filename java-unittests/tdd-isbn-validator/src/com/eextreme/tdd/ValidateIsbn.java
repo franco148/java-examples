60eeab2190a389fbbd88615cf8fa960d6654b1e3
franco-extreme <franck_ral@hotmail.com>
2018-11-25 22:45:52 -0400

    JavaExamples-TddImplementation: What is a GOOD or BAD test?.

--- java-unittests/tdd-isbn-validator/src/com/eextreme/tdd/ValidateIsbn.java
+++ java-unittests/tdd-isbn-validator/src/com/eextreme/tdd/ValidateIsbn.java
@@ -6,6 +6,29 @@ package com.eextreme.tdd;
  * 3. Sometimes we know that our code is going to be like an rubbish code. It is not what we want, there is not sensible logic in it at all. But the code should
  *    made the tests pass. So it is the right thing today for the purposes of test driven development.
  *    If we are not going to try and do any kind of shortcuts let's save and run this test again.
+ *    
+ *    
+ *    WHAT IS A "GOOD" OR "BAD" TEST?
+ * 1. A new test will initially fail.
+ * 2. We work to get the test to pass.
+ * 3. We finally optimize our code, and tests   
+ * 
+ * - TEST ONE ITEM OF FUNCTIONALITY ONLY: Methods should normally have a single assert you only use more than one assert in a test method
+ *   if you are either testing two or more examples of exactly the same thing or you need to validate more that one value in order to know
+ *   that the test has worked.
+ * - TEST BUSINESS LOGIC, NOT METHODS: Now this is an important feature of test driven development. In a larger system your classes may have lots
+ *   and lots of methods but you do not write the tests to check that the methods work, you write tests to check that the business logic is correct.
+ *   You might need to call lots of methods in your application in a single test to check business logic.
+ *   In other words there will not be normally a one to one relationship between your tests and your methods, if you are naming a test with a name
+ *   that almost exactly matches one of your method names then this could be a warning that you might be forgetting to test business logic
+ *   not methods.  
+ * - TESTS MUST BE REPEATABLE: 
+ *    
+ *    
+ *    
+ *    
+ *    
+ *    
  */
 public class ValidateIsbn {
 
