dceb0b5158dc8213defb44b87004047cae7900d1
franco-extreme <franck_ral@hotmail.com>
2018-11-26 23:36:18 -0400

    JavaExamples-TddImplementation: Setting up Mockito and creating a mock.

--- java-unittests/tdd-isbn-validator/test/com/eextreme/tdd/StockManagementTests.java
+++ java-unittests/tdd-isbn-validator/test/com/eextreme/tdd/StockManagementTests.java
@@ -3,6 +3,7 @@ package com.eextreme.tdd;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 public class StockManagementTests {
 
@@ -32,6 +33,30 @@ public class StockManagementTests {
 		String isbn = "0140177396";		
 		String locatorCode = stockManager.getLocatorCode(isbn);
 		assertEquals("7396J4", locatorCode);
+	}
+	
+	/**
+	 * Here we need another concept and this is called a MOCK. A mock is similar to a stub we've already built
+	 * but with a mark that we get additional features. We get to find out whether or not a method was caught so we
+	 * can write a line of code.
+	 */
+	@Test
+	public void databaseIsUsedIfDataIsPresent() {
+		ExternalIsbnDataService databaseService = mock(ExternalIsbnDataService.class);
+		ExternalIsbnDataService webService = mock(ExternalIsbnDataService.class);
+		
+		StockManager stockManager = new StockManager();
+		stockManager.setWebService(webService);
+		stockManager.setDbService(databaseService);
+		
+		String isbn = "0140177396";		
+		String locatorCode = stockManager.getLocatorCode(isbn);
+		assertEquals("7396J4", locatorCode);
+	}
+	
+	@Test
+	public void webServerIsUsedIfDataIsNotPresentInDatabase() {
+		fail();
 	}
 
 }
