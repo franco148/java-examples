437ab8dc7e5b215b05802e066af184e11455f7c4
Franco Arratia <franck_ral@hotmail.com>
2019-01-22 10:50:58 -0400

    Java-JUnit5: Initial test with JUnit 5

---
+++ java-unittests/JUnit5/test/com/fral/junit5/StringTest.java
@@ -0,0 +1,13 @@
+package com.fral.junit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class StringTest {
+
+    @Test
+    public void testMethod() {
+        fail("Initial unit test with junit5");
+    }
+}
