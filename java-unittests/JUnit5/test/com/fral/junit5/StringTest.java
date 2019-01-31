7aed3f8e5e0ad0aeb39f80fd0bb7e1d653d6b4fa
Franco Arratia <franck_ral@hotmail.com>
2019-01-31 12:49:24 -0400

    Java-JUnit5: Writing Assertions for Arrays.

--- java-unittests/JUnit5/test/com/fral/junit5/StringTest.java
+++ java-unittests/JUnit5/test/com/fral/junit5/StringTest.java
@@ -2,12 +2,16 @@ package com.fral.junit5;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringTest {
 
     @Test
-    public void testMethod() {
-        fail("Initial unit test with junit5");
+    public void split_basic() {
+        String str = "abc def ghi";
+        String actualResult[] = str.split(" ");
+        String[] expectedResult = new String[] { "abc", "def", "ghi" };
+
+        assertArrayEquals(expectedResult, actualResult);
     }
 }
