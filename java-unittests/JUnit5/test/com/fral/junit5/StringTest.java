4bbb516a39ab271c738a31ce62968535d79bb888
Franco Arratia <franck_ral@hotmail.com>
2019-01-31 12:57:30 -0400

    Java-JUnit5: Setting up data for every test @BeforeEach, @AfterEach, @BeforeAll, @AfterAll.

--- java-unittests/JUnit5/test/com/fral/junit5/StringTest.java
+++ java-unittests/JUnit5/test/com/fral/junit5/StringTest.java
@@ -1,10 +1,31 @@
 package com.fral.junit5;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringTest {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all...");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each...");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each...");
+    }
 
     @Test
     public void split_basic() {
@@ -13,5 +34,14 @@ public class StringTest {
         String[] expectedResult = new String[] { "abc", "def", "ghi" };
 
         assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void length_basic() {
+        String str = "abc def ghi";
+        int actualResult = str.length();
+        int expectedResult = 11;
+
+        assertEquals(expectedResult, actualResult);
     }
 }
