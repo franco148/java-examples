fb4295e929aa635294222fcbfa2f76537dc5e9c3
franco-extreme <franck_ral@hotmail.com>
2019-03-10 18:55:50 -0400

    Java-Threads: A worked example using low-level synchronization.

---
+++ threading/src/threading/demo/waitnotify/AppSync.java
@@ -0,0 +1,37 @@
+package threading.demo.waitnotify;
+
+public class AppSync {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final ProcessorSync processor = new ProcessorSync();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.consume();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+}
