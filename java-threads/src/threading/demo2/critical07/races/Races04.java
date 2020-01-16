cedeeb44e4891a90786e62341b55c6a391964988
Franco Arratia <franck_ral@hotmail.com>
2020-01-16 17:13:15 -0400

    Java-Threads: Data Races concepts

--- java-threads/src/threading/demo2/critical07/races/Races04.java
+++ java-threads/src/threading/demo2/critical07/races/Races04.java
@@ -2,6 +2,29 @@ package threading.demo2.critical07.races;
 
 /**
  * Race Conditions & Data Races
+ *
+ * Race Condition
+ * - Condition when multiple threads are accesssing a shared resource.
+ * - At least one thread is modifying the resource.
+ * - The timing of threads'scheduling may cause incorrect results.
+ * - The core of the problem is non atomic operations performed on the shared resource.
+ *
+ * Data Race - Problem
+ * - Compiler and CPU may execute the instructions Out Of Order to optimize performance and utilization.
+ * - They will do so while maintaining the logical correctness of the code.
+ * - Out of Order execution by the compiler the compiler and CPU are important features to speed up the code.
+ * - The compiler re-arranges instructions for better
+ * --- Branch predication (optimized loops, "if" statements etc.)
+ * --- Vectorization - parallel instruction execution (SIMD)
+ * --- Prefetching instructions - better cache performance
+ * - CPU re-arranges instructions for better hardware units utilization.
+ *
+ *
+ * Data Race - Solutions
+ * Establish a happens - before semantics by one of these methods:
+ * - Synchronization of methods which modify shared variables
+ * - Declaration of shared variables with the volatile keyword
+ *
  */
 public class Races04 {
 
